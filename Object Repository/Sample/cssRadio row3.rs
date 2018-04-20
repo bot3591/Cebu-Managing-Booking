be4abcd4e8b94903182c6267ece0937de92d6e3a
santosr <santosr@SANTOSP.caimsc.compaid.com>
2018-04-20 10:19:58 +0800

    Sample Text

---
+++ Object Repository/Sample/cssRadio row3.rs
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>cssRadio row3</name>
+   <tag></tag>
+   <elementGuidId>ee89dc33-28b8-4eb4-b44e-f64801250932</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;trip_0_date_0_flight_0_fare_11&quot;)[count(. | //div[@id = 'trip_0_date_0_flight_0_fare_11']) = count(//div[@id = 'trip_0_date_0_flight_0_fare_11'])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>cssRadio row3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value></value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;trip_0_date_0_flight_0_fare_11&quot;)</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>trip_0_date_0_flight_0_fare_11</value>
+   </webElementProperties>
+</WebElementEntity>
